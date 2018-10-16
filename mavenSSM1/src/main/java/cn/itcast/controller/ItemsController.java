6943e678a03f5128a44b2eac36792895e3e31f0b
root <123@qq.com>
2018-10-16 21:52:39 +0800

    第五次修改

--- mavenSSM1/src/main/java/cn/itcast/controller/ItemsController.java
+++ mavenSSM1/src/main/java/cn/itcast/controller/ItemsController.java
@@ -21,6 +21,7 @@ public class ItemsController {
             System.out.println(items);
         }
         System.out.println("git测试");
+        System.out.println("git测试4");
         return "success";
     }
 }
